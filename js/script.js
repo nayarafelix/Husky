d6e3a549407ea3981329437f65213ee023708e5c
Nayara <nayara.pereira.felix@gmail.com>
2018-09-16 21:19:59 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function btnMenu() {
+  menu = document.getElementsByClassName('menu');
+  menu[0].style.display = 'block';
+}
+function btnFechar() {
+  menu = document.getElementsByClassName('menu');
+  menu[0].style.display = 'none';
+}
